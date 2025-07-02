fd06b3e0442da0417ba1d182143788dd4af41de4
p1 <sandeepsandy14310@gmail.com>
2025-07-02 17:27:59 +0530

    changed the script

---
+++ 03-data -type.sh
@@ -0,0 +1,8 @@
+#!bin/bash/
+
+# we creating sum of 2 number
+
+Number1=1$
+Number2=2$
+
+echo
---
+++ 04-variable.sh
@@ -0,0 +1,9 @@
+#!bin/bash/
+
+Number1=rahim
+number2=rahul
+
+echo "number1: hello number2!"
+echo "number2: hello number1!"
+echo "number1: how are you today!"
+echo "number2": iam good!"
